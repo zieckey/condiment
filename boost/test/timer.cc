485eeff62f7eacabb5f7281537f20664e06a1a34
weizili.laptop <zieckey@gmail.com>
2015-04-21 18:44:40 +0800

    add timer.cc

--- boost/test/timer.cc
+++ boost/test/timer.cc
@@ -5,6 +5,20 @@
 
 TEST_UNIT(timer_test)
 {
-
+    boost::timer t;
+    std::cout << t.elapsed() << "s\n";
+    std::cout << t.elapsed() << "s\n";
+    std::cout << t.elapsed() << "s\n";
+    std::cout << t.elapsed() << "s\n";
 }
 
+
+
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+TEST_UNIT(time_duration_test)
+{
+    boost::posix_time::time_duration t;
+    std::cout << "sizeof(boost::posix_time::time_duration)=" << sizeof(t) << std::endl;
+}
