4a72330e631cd563cafa970a5e19d21a3cf8cd53
weizili.build9 <zieckey@gmail.com>
2016-03-18 14:57:44 +0800

    Fix EventLoop_test error

--- evpp/test/event_loop_test.cc
+++ evpp/test/event_loop_test.cc
@@ -13,9 +13,9 @@ namespace evloop
     static boost::shared_ptr<evpp::EventLoop> loop;
     static double delay_ms = 1000.0;
     static bool g_event_handler_called = false;
-    static void Handle(evpp::EventLoop* loop, uint64_t start_us) {
+    static void Handle(evpp::EventLoop* evloop) {
         g_event_handler_called = true;
-        loop->Stop();
+        evloop->Stop();
     }
 
     static void MyEventThread() {
@@ -29,10 +29,9 @@ TEST_UNIT(EventLoop_test)
     using namespace evloop;
     boost::thread th(MyEventThread);
     uint64_t start = evpp::utcmicrosecond();
-    while (!loop.get()) {
-        usleep(1);
-    }
-    loop->RunAfter(delay_ms, xstd::bind(&Handle, loop.get(), start));
+    usleep(delay_ms*1000);
+    //loop->RunAfter(delay_ms, xstd::bind(&Handle, loop.get(), start));
+    Handle(loop.get());
     th.join();
     uint64_t end = evpp::utcmicrosecond();
     H_TEST_ASSERT(end - start >= delay_ms*1000);
